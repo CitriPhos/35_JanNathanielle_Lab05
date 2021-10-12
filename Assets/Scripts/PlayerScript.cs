2ac7a3cb700e86e041486a695024cbf54c267bf6
CitriPhos <jan.nathanielle_strachan@yahoo.com>
2021-10-12 16:31:34 +0800

    Created PlayerScript
    - Implemented collision of coin & water w/ player
    - Added Score UI text

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+    private float score;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Coin"))
+        {
+            score += 10;
+            Destroy(other.gameObject);
+            scoreText.text = $"Score: {score}";
+        }
+
+        if (other.gameObject.tag.Equals("Water"))
+        {
+            SceneManager.LoadScene("GameLoseScene");
+        }
+    }
+}
